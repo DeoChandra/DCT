d6f04c2be9013ef8e55f41d5e87766ee761a4128
DeoChandra <deochandra>
2018-03-25 21:36:01 +0530

    First commit

---
+++ Assignments/Project4.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Project4.aspx.cs" Inherits="Assignments.Project4" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+       
+        <asp:TextBox ID="txtData" runat="server"></asp:TextBox> <br /><br />
+        <asp:Button ID="btnGetData" runat="server" Text="Get Data" 
+            onclick="btnGetData_Click" /> <br /><br />
+         <asp:GridView ID="gvwData"
+            runat="server">
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Assignments/Project4.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Assignments
+{
+    public partial class Project4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+            }
+        }
+
+        public List<string> get_the_recs(string rec)
+        {
+            List<string> result = null;
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["assignment"].ToString()))
+                {
+                    using (SqlCommand cmd = new SqlCommand("SELECT comment FROM tb_the_comments INNER JOIN tb_the_recs ON rec_the_ID = ID WHERE rec= '" + rec + "'", cn))
+                    {
+                        cn.Open();
+                        SqlDataReader rd = cmd.ExecuteReader();
+                        if (rd.HasRows)
+                        {
+                            result = new List<string>();
+                            while (rd.Read())
+                            {
+                                result.Add(rd["comment"].ToString());
+                            }
+                            rd.Close();
+                            cn.Close();                            
+                        }
+                        else
+                        {
+                            cn.Close();                            
+                        }
+                    }
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+                return null;                
+            }           
+        }
+
+        protected void btnGetData_Click(object sender, EventArgs e)
+        {
+            List<string> result = get_the_recs(txtData.Text.Replace("'", "''").Trim());
+            gvwData.DataSource = result;
+            gvwData.DataBind();
+        }
+    }
+}
